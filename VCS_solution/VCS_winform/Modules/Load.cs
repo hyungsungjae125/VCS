15419fbbaad9629f2064597440dee83dfbc28d7a
형성재 <tldwo125@naver.com>
2019-03-04 10:02:47 +0900

    솔루션 생성 및 기본 폼 구성만들기

---
+++ VCS_solution/VCS_winform/Modules/Load.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VCS_winform.Modules
+{
+    class Load
+    {
+        private Form target;//MDI부분에서 타겟폼/현재 띄워주고 싶은 폼
+        private Form parent;//MDI부분에서 부모폼
+        public Load(Form target)
+        {
+            this.target = target;
+        }
+
+        
+    }
+}
